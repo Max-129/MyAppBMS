181c09b2641eae40647e44736ef60dca7f2a81e8
Max-129 <sadvibe129@gmail.com>
2023-02-22 16:48:45 +0600

    HomeWork #3.1

--- app/src/main/java/com/example/myappbms/taskmanager/data/local/Pref.kt
+++ app/src/main/java/com/example/myappbms/taskmanager/data/local/Pref.kt
@@ -5,14 +5,25 @@ import android.content.Context.MODE_PRIVATE
 
 class Pref(context: Context) {
     private val pref = context.getSharedPreferences(PREF_NAME, MODE_PRIVATE)
+
     fun isUserSeen(): Boolean {
         return pref.getBoolean(SEEN_KEY, false)
     }
     fun saveSeen(){
         pref.edit().putBoolean(SEEN_KEY, true).apply()
     }
+
+    fun saveName(name: String){
+        pref.edit().putString(NAME_KEY, name).apply()
+    }
+
+    fun getName(): String {
+        return pref.getString(NAME_KEY, "").toString()
+    }
+
     companion object {
         const val PREF_NAME = "pref.task"
         const val SEEN_KEY = "seen.key"
+        const val NAME_KEY = "name.key"
     }
 }
--- app/src/main/java/com/example/myappbms/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/myappbms/ui/profile/ProfileFragment.kt
@@ -6,14 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.myappbms.R
+import com.example.myappbms.databinding.FragmentProfileBinding
+import com.example.myappbms.taskmanager.data.local.Pref
 
 
 class ProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
+    private lateinit var pref: Pref
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        pref = Pref(requireContext())
+        binding.edName.setText(pref.getName()).toString()
+        pref.saveName(binding.edName.text.toString())
+    }
+
 }
