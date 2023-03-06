5641dd68c25a56191909e310156dff080dc5f321
Max-129 <sadvibe129@gmail.com>
2023-03-06 15:17:13 +0600

    HomeWork #5

--- app/src/main/java/com/example/myappbms/utils/Exteption.kt
+++ app/src/main/java/com/example/myappbms/utils/Exteption.kt
@@ -3,6 +3,7 @@ package com.example.myappbms.utils
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 
+
 fun ImageView.loadImage(url: String) {
 //    Picasso.get().load(url).into(this)
     Glide.with(this).load(url).into(this)
